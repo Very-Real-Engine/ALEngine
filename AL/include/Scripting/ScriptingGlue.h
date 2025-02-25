e77c6d4a5932d9dc2bbaeedc679b4fbd72080b5b
jgoo <maruluv0527@gmail.com>
2025-02-25 15:24:39 +0900

    [Feat]: ScriptingGlue header doxygen added

--- AL/include/Scripting/ScriptingGlue.h
+++ AL/include/Scripting/ScriptingGlue.h
@@ -2,10 +2,18 @@
 
 namespace ale
 {
+/**
+ * @class ScriptGlue
+ * @brief C# 스크립트와 C++ 엔진을 연결하는 클래스.
+ *        엔진의 컴포넌트 및 함수를 C#에서 사용할 수 있도록 등록하는 역할을 수행.
+ */
 class ScriptGlue
 {
   public:
+	/// @brief 엔진의 컴포넌트를 C#에서 사용할 수 있도록 등록합니다.
 	static void registerComponents();
+
+	/// @brief 엔진의 특정 함수를 C#에서 호출할 수 있도록 등록합니다.
 	static void registerFunctions();
 };
 } // namespace ale
