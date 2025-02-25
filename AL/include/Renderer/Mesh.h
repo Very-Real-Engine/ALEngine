234285ced94b6793a92a3958457124c5424dac58
surkim <ksro0128@naver.com>
2025-02-26 07:36:51 +0900

    [Feat]: add doxygen renderer

--- AL/include/Renderer/Mesh.h
+++ AL/include/Renderer/Mesh.h
@@ -1,5 +1,9 @@
-#ifndef MESH_H
-#define MESH_H
+#pragma once
+
+/**
+ * @file Mesh.h
+ * @brief Mesh 클래스
+ */
 
 #include "Core/Base.h"
 #include "Renderer/Buffer.h"
@@ -7,26 +11,81 @@
 
 namespace ale
 {
+/**
+ * @brief Mesh 클래스
+ */
 class Mesh
 {
   public:
+	/**
+	 * @brief Mesh 생성
+	 * @param vertices 정점 목록
+	 * @param indices 인덱스 목록
+	 * @return Mesh
+	 */
 	static std::shared_ptr<Mesh> createMesh(std::vector<Vertex> &vertices, std::vector<uint32_t> &indices);
+	/**
+	 * @brief 박스 생성
+	 * @return 박스
+	 */
 	static std::shared_ptr<Mesh> createBox();
+	/**
+	 * @brief 구 생성
+	 * @return 구
+	 */
 	static std::shared_ptr<Mesh> createSphere();
+	/**
+	 * @brief 평면 생성
+	 * @return 평면
+	 */
 	static std::shared_ptr<Mesh> createPlane();
+	/**
+	 * @brief 지면 생성
+	 * @return 지면
+	 */
 	static std::shared_ptr<Mesh> createGround();
+	/**
+	 * @brief 캡슐 생성
+	 * @return 캡슐
+	 */
 	static std::shared_ptr<Mesh> createCapsule();
+	/**
+	 * @brief 원기둥 생성
+	 * @return 원기둥
+	 */
 	static std::shared_ptr<Mesh> createCylinder();
+	/**
+	 * @brief 콜라이더 박스 생성
+	 * @return 콜라이더 박스
+	 */
 	static std::shared_ptr<Mesh> createColliderBox();
 
 	~Mesh() = default;
 
+	/**
+	 * @brief Mesh 정리
+	 */
 	void cleanup();
 
+	/**
+	 * @brief Mesh 그리기
+	 * @param commandBuffer 명령 버퍼
+	 */
 	void draw(VkCommandBuffer commandBuffer);
-
+	/**
+	 * @brief AABB 계산
+	 * @param vertices 정점 목록
+	 */
 	void calculateAABB(std::vector<Vertex> &vertices);
+	/**
+	 * @brief 최대 위치 반환
+	 * @return 최대 위치
+	 */
 	alglm::vec3 getMaxPos();
+	/**
+	 * @brief 최소 위치 반환
+	 * @return 최소 위치
+	 */
 	alglm::vec3 getMinPos();
 
   private:
@@ -37,9 +96,17 @@ class Mesh
 	std::unique_ptr<VertexBuffer> m_vertexBuffer;
 	std::unique_ptr<IndexBuffer> m_indexBuffer;
 
+	/**
+	 * @brief Mesh 초기화
+	 * @param vertices 정점 목록
+	 * @param indices 인덱스 목록
+	 */
 	void initMesh(std::vector<Vertex> &vertices, std::vector<uint32_t> &indices);
+	/**
+	 * @brief 탄젠트 계산
+	 * @param vertices 정점 목록
+	 * @param indices 인덱스 목록
+	 */
 	void calculateTangents(std::vector<Vertex> &vertices, const std::vector<uint32_t> &indices);
 };
 } // namespace ale
-
-#endif
--- AL/include/Renderer/SyncObjects.h
+++ AL/include/Renderer/SyncObjects.h
@@ -1,5 +1,9 @@
-#ifndef SYNCOBJECTS_H
-#define SYNCOBJECTS_H
+#pragma once
+
+/**
+ * @file SyncObjects.h
+ * @brief 동기화 객체 클래스
+ */
 
 #include "Core/Base.h"
 #include "Renderer/Common.h"
@@ -7,23 +11,46 @@
 
 namespace ale
 {
+/**
+ * @class SyncObjects
+ * @brief 동기화 객체 클래스
+ */
 class SyncObjects
 {
   public:
+	/**
+	 * @brief 동기화 객체 생성
+	 * @return std::unique_ptr<SyncObjects> 동기화 객체
+	 */
 	static std::unique_ptr<SyncObjects> createSyncObjects();
-
+	/**
+	 * @brief 동기화 객체 정리
+	 */
 	~SyncObjects() = default;
-
+	/**
+	 * @brief 동기화 객체 정리
+	 */
 	void cleanup();
-
+	/**
+	 * @brief 이미지 사용 가능 세마포어 반환
+	 * @return std::vector<VkSemaphore> 이미지 사용 가능 세마포어
+	 */
 	std::vector<VkSemaphore> &getImageAvailableSemaphores()
 	{
 		return imageAvailableSemaphores;
 	}
+	/**
+	 * @brief 렌더 완료 세마포어 반환
+	 * @return std::vector<VkSemaphore> 렌더 완료 세마포어
+	 */
 	std::vector<VkSemaphore> &getRenderFinishedSemaphores()
 	{
 		return renderFinishedSemaphores;
 	}
+	/**
+	 * @brief 렌더 완료 세마포어 반환
+	 * @return std::vector<VkFence> 렌더 완료 세마포어
+	 */
 	std::vector<VkFence> &getInFlightFences()
 	{
 		return inFlightFences;
@@ -33,9 +60,9 @@ class SyncObjects
 	std::vector<VkSemaphore> imageAvailableSemaphores;
 	std::vector<VkSemaphore> renderFinishedSemaphores;
 	std::vector<VkFence> inFlightFences;
-
+	/**
+	 * @brief 동기화 객체 초기화
+	 */
 	void initSyncObjects();
 };
 } // namespace ale
-
-#endif
