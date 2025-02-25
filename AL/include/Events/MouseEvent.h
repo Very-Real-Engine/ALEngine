7ccb613c439d88724cf164d6555722a044925021
jgoo <maruluv0527@gmail.com>
2025-02-25 15:22:34 +0900

    [Feat]: Event header doxygen added

--- AL/include/Events/MouseEvent.h
+++ AL/include/Events/MouseEvent.h
@@ -1,28 +1,48 @@
-#ifndef MOUSEEVENT_H
-#define MOUSEEVENT_H
+#pragma once
 
 #include "Core/MouseCodes.h"
 #include "Events/Event.h"
 
 namespace ale
 {
+/**
+ * @class MouseMovedEvent
+ * @brief 마우스가 이동했을 때 발생하는 이벤트.
+ */
 class MouseMovedEvent : public Event
 {
   public:
+	/**
+	 * @brief MouseMovedEvent 생성자.
+	 * @param x 마우스의 X 좌표.
+	 * @param y 마우스의 Y 좌표.
+	 */
 	MouseMovedEvent(const float x, const float y) : m_MouseX(x), m_MouseY(y)
 	{
 	}
 
+	/**
+	 * @brief 마우스의 X 좌표를 반환합니다.
+	 * @return float 현재 마우스의 X 좌표.
+	 */
 	float getX() const
 	{
 		return m_MouseX;
 	}
 
+	/**
+	 * @brief 마우스의 Y 좌표를 반환합니다.
+	 * @return float 현재 마우스의 Y 좌표.
+	 */
 	float getY() const
 	{
 		return m_MouseY;
 	}
 
+	/**
+	 * @brief 이벤트 정보를 문자열로 변환합니다.
+	 * @return std::string "MouseMovedEvent: x, y" 형식의 문자열.
+	 */
 	std::string toString() const override
 	{
 		std::stringstream ss;
@@ -37,23 +57,44 @@ class MouseMovedEvent : public Event
 	float m_MouseX, m_MouseY;
 };
 
+/**
+ * @class MouseScrolledEvent
+ * @brief 마우스가 스크롤될 때 발생하는 이벤트.
+ */
 class MouseScrolledEvent : public Event
 {
   public:
+	/**
+	 * @brief MouseScrolledEvent 생성자.
+	 * @param xOffset X축 스크롤 오프셋.
+	 * @param yOffset Y축 스크롤 오프셋.
+	 */
 	MouseScrolledEvent(const float xOffset, const float yOffset) : m_XOffset(xOffset), m_YOffset(yOffset)
 	{
 	}
 
+	/**
+	 * @brief 마우스 X축 스크롤 오프셋을 반환합니다.
+	 * @return float X축 스크롤 값.
+	 */
 	float getXOffset() const
 	{
 		return m_XOffset;
 	}
 
+	/**
+	 * @brief 마우스 Y축 스크롤 오프셋을 반환합니다.
+	 * @return float Y축 스크롤 값.
+	 */
 	float getYOffset() const
 	{
 		return m_YOffset;
 	}
 
+	/**
+	 * @brief 이벤트 정보를 문자열로 변환합니다.
+	 * @return std::string "MouseScrolledEvent: xOffset, yOffset" 형식의 문자열.
+	 */
 	std::string toString() const override
 	{
 		std::stringstream ss;
@@ -68,9 +109,17 @@ class MouseScrolledEvent : public Event
 	float m_XOffset, m_YOffset;
 };
 
+/**
+ * @class MouseButtonEvent
+ * @brief 마우스 버튼과 관련된 이벤트의 기본 클래스.
+ */
 class MouseButtonEvent : public Event
 {
   public:
+	/**
+	 * @brief 눌린 마우스 버튼을 반환합니다.
+	 * @return MouseCode 마우스 버튼 코드.
+	 */
 	MouseCode getMouseButton() const
 	{
 		return m_Button;
@@ -79,6 +128,10 @@ class MouseButtonEvent : public Event
 	EVENT_CLASS_CATEGORY(EVENT_CATEGORY_MOUSE | EVENT_CATEGORY_INPUT | EVENT_CATEGORY_MOUSE_BUTTON);
 
   protected:
+	/**
+	 * @brief MouseButtonEvent 생성자.
+	 * @param button 이벤트가 발생한 마우스 버튼 코드.
+	 */
 	MouseButtonEvent(const MouseCode button) : m_Button(button)
 	{
 	}
@@ -86,13 +139,25 @@ class MouseButtonEvent : public Event
 	MouseCode m_Button;
 };
 
+/**
+ * @class MouseButtonPressedEvent
+ * @brief 마우스 버튼이 눌렸을 때 발생하는 이벤트.
+ */
 class MouseButtonPressedEvent : public MouseButtonEvent
 {
   public:
+	/**
+	 * @brief MouseButtonPressedEvent 생성자.
+	 * @param button 눌린 마우스 버튼 코드.
+	 */
 	MouseButtonPressedEvent(const MouseCode button) : MouseButtonEvent(button)
 	{
 	}
 
+	/**
+	 * @brief 이벤트 정보를 문자열로 변환합니다.
+	 * @return std::string "MouseButtonPressedEvent: button" 형식의 문자열.
+	 */
 	std::string toString() const override
 	{
 		std::stringstream ss;
@@ -103,13 +168,25 @@ class MouseButtonPressedEvent : public MouseButtonEvent
 	EVENT_CLASS_TYPE(MOUSE_BUTTON_PRESSED);
 };
 
+/**
+ * @class MouseButtonReleasedEvent
+ * @brief 마우스 버튼이 떼어졌을 때 발생하는 이벤트.
+ */
 class MouseButtonReleasedEvent : public MouseButtonEvent
 {
   public:
+	/**
+	 * @brief MouseButtonReleasedEvent 생성자.
+	 * @param button 떼어진 마우스 버튼 코드.
+	 */
 	MouseButtonReleasedEvent(const MouseCode button) : MouseButtonEvent(button)
 	{
 	}
 
+	/**
+	 * @brief 이벤트 정보를 문자열로 변환합니다.
+	 * @return std::string "MouseButtonReleasedEvent: button" 형식의 문자열.
+	 */
 	std::string toString() const override
 	{
 		std::stringstream ss;
@@ -121,5 +198,3 @@ class MouseButtonReleasedEvent : public MouseButtonEvent
 };
 
 } // namespace ale
-
-#endif
